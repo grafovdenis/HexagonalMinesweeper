15ecfecc7a9625f4ad76edbd8fdfd8468a36903c
Grafov Denis <grafov.denis@gmail.com>
2017-06-01 01:39:42 +0300

    fixes

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import static java.lang.Math.sqrt;
 
 
 public class Main extends Application {
 
-    private static final int TILE_SIZE = 80;
+    static final int TILE_SIZE = 80;
     private static final int W = 800 + TILE_SIZE / 2;
     private static final int H = 800;
-    private static double SIDE = TILE_SIZE / sqrt(3);
 
-    private static final int X_TILES = W / TILE_SIZE;
-    private static final int Y_TILES = H / TILE_SIZE;
+    static final int X_TILES = W / TILE_SIZE;
+    static final int Y_TILES = H / TILE_SIZE;
 
-    private Tile[][] grid = new Tile[X_TILES][Y_TILES];
-    private Scene scene;
+    static Tile[][] grid = new Tile[X_TILES][Y_TILES];
+    static Scene scene;
 
-    private Parent createContent() {
+    static Parent createContent() {
         Pane root = new Pane();
         root.setPrefSize(W, H);
 
@@ -48,7 +38,7 @@ public class Main extends Application {
                 if (tile.hasBomb)
                     continue;
 
-                long bombs = getNeighbors(tile).stream().filter(t -> t.hasBomb).count();
+                long bombs = Tile.getNeighbors(tile).stream().filter(t -> t.hasBomb).count();
 
                 if (bombs > 0)
                     tile.text.setText(String.valueOf(bombs));
@@ -56,109 +46,6 @@ public class Main extends Application {
         }
 
         return root;
-    }
-
-    private List<Tile> getNeighbors(Tile tile) {
-        List<Tile> neighbors = new ArrayList<>();
-
-        //  t t
-        // t X t
-        //  t t
-
-        int[] points = (tile.y % 2 != 0) ? new int[]{
-                0, -1,
-                1, -1,
-                -1, 0,
-                1, 0,
-                0, 1,
-                1, 1
-        } : new int[]{
-                -1, -1,
-                0, -1,
-                -1, 0,
-                1, 0,
-                -1, 1,
-                0, 1
-        };
-
-        for (int i = 0; i < points.length; i++) {
-            int dx = points[i];
-            int dy = points[++i];
-
-            double newX = tile.x + dx;
-            double newY = tile.y + dy;
-
-            if (newX >= 0 && newX < X_TILES
-                    && newY >= 0 && newY < Y_TILES) {
-                neighbors.add(grid[(int) newX][(int) newY]);
-            }
-        }
-
-        return neighbors;
-    }
-
-    private class Tile extends StackPane {
-        private double y;
-        private double x;
-        private boolean hasBomb;
-        private boolean isOpen = false;
-        private Polygon border = new Polygon(
-                0, (TILE_SIZE - SIDE) / 2,
-                TILE_SIZE / 2, 0,
-                TILE_SIZE, (TILE_SIZE - SIDE) / 2,
-                TILE_SIZE, (TILE_SIZE - SIDE) / 2 + SIDE,
-                TILE_SIZE / 2, TILE_SIZE,
-                0, (TILE_SIZE - SIDE) / 2 + SIDE);
-        private Text text = new Text();
-
-
-        Tile(double x, double y, boolean hasBomb) {
-            this.x = x;
-            this.y = y;
-            this.hasBomb = hasBomb;
-
-            border.setStroke(Color.LIGHTGRAY);
-            border.setFill(Color.YELLOWGREEN);
-
-            text.setFont(Font.font(18));
-            text.setFill(Color.BLACK);
-            text.setText(hasBomb ? "X" : "");
-            text.setVisible(false);
-
-            getChildren().addAll(border, text);
-
-
-            double curX = (y % 2 != 0) ? x + 0.5 : x;
-
-            setTranslateX(curX * TILE_SIZE);
-            setTranslateY(y * 0.8 * TILE_SIZE);
-            setOnMouseReleased(e -> flagged());
-            setOnMouseClicked(e -> open());
-        }
-
-        void flagged() {
-            if (!isOpen)
-                border.setFill(Color.RED);
-        }
-
-        void open() {
-            if (isOpen)
-                return;
-
-            if (hasBomb) {
-                System.out.println("Game Over");
-                scene.setRoot(createContent());
-                return;
-            }
-
-            isOpen = true;
-            text.setVisible(true);
-            border.setFill(Color.ORANGE);
-
-            if (text.getText().isEmpty()) {
-                getNeighbors(this).forEach(Tile::open);
-            }
-        }
     }
 
     @Override
@@ -169,6 +56,7 @@ public class Main extends Application {
         stage.show();
         stage.setTitle("Minesweeper");
         stage.getIcons().add(new Image("icon.png"));
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
---
+++ src/Tile.java
@@ -0,0 +1,137 @@
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.sqrt;
+
+class Tile extends StackPane {
+    private double y;
+    private double x;
+    boolean hasBomb;
+    private boolean isOpen = false;
+    private static final int TILE_SIZE = Main.TILE_SIZE;
+    private static double SIDE = TILE_SIZE / sqrt(3);
+    private Polygon border = new Polygon(
+            0, (TILE_SIZE - SIDE) / 2,
+            TILE_SIZE / 2, 0,
+            TILE_SIZE, (TILE_SIZE - SIDE) / 2,
+            TILE_SIZE, (TILE_SIZE - SIDE) / 2 + SIDE,
+            TILE_SIZE / 2, TILE_SIZE,
+            0, (TILE_SIZE - SIDE) / 2 + SIDE);
+    Text text = new Text();
+
+
+    Tile(double x, double y, boolean hasBomb) {
+        this.x = x;
+        this.y = y;
+        this.hasBomb = hasBomb;
+
+        border.setStroke(Color.LIGHTGRAY);
+        border.setFill(Color.YELLOWGREEN);
+
+        text.setFont(Font.font(18));
+        text.setFill(Color.BLACK);
+        text.setText(hasBomb ? "X" : "");
+        text.setVisible(false);
+
+        getChildren().addAll(border, text);
+
+
+        double curX = (y % 2 != 0) ? x + 0.5 : x;
+
+        setTranslateX(curX * TILE_SIZE);
+        setTranslateY(y * 0.8 * TILE_SIZE);
+
+        setOnMousePressed(e -> {
+            if (e.isPrimaryButtonDown()) {
+                open();
+            }
+            if (e.isSecondaryButtonDown()) {
+                flagged();
+            }
+        });
+    }
+
+    private void flagged() {
+        if (!isOpen)
+            border.setFill(Color.RED);
+    }
+
+    static List<Tile> getNeighbors(Tile tile) {
+        List<Tile> neighbors = new ArrayList<>();
+
+        //  t t
+        // t X t
+        //  t t
+
+        int[] points = (tile.y % 2 != 0) ? new int[]{
+                0, -1,
+                1, -1,
+                -1, 0,
+                1, 0,
+                0, 1,
+                1, 1
+        } : new int[]{
+                -1, -1,
+                0, -1,
+                -1, 0,
+                1, 0,
+                -1, 1,
+                0, 1
+        };
+
+        for (int i = 0; i < points.length; i++) {
+            int dx = points[i];
+            int dy = points[++i];
+
+            double newX = tile.x + dx;
+            double newY = tile.y + dy;
+
+            if (newX >= 0 && newX < Main.X_TILES
+                    && newY >= 0 && newY < Main.Y_TILES) {
+                neighbors.add(Main.grid[(int) newX][(int) newY]);
+            }
+        }
+
+        return neighbors;
+    }
+
+    private void open() {
+        if (isOpen)
+            return;
+
+        if (hasBomb) {
+            //open all
+            for (Tile[] tiles: Main.grid) {
+                for (Tile tile: tiles) {
+                    tile.text.setVisible(true);
+                }
+            }
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("You Lose");
+            alert.setHeaderText(null);
+            alert.setContentText("Try Again!");
+            alert.showAndWait();
+
+            Main.scene.setRoot(Main.createContent());
+            return;
+        }
+
+        isOpen = true;
+        text.setVisible(true);
+        border.setFill(Color.ORANGE);
+
+        if (text.getText().isEmpty()) {
+            getNeighbors(this).forEach(Tile::open);
+        }
+    }
+}
